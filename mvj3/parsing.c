ca3a903a89603abd32189bbe71f7c9805535a76c
David Chen <mvjome@gmail.com>
2014-05-03 14:40:04 +0800

    cleanup builtin_head and builtin_tail with MACRO

--- mvj3/parsing.c
+++ mvj3/parsing.c
@@ -6,6 +6,8 @@
 #include <editline/readline.h>
 #include <histedit.h>
 
+#define LASSERT(args, cond, err) if (!(cond)) { lval_del(args); return lval_err(err); }
+
 /* Create Enumeration of Possible lval Types */
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 
@@ -194,20 +196,11 @@ lval* builtin_op(lval* a, char* op) {
 
 lval* builtin_head(lval* a) {
   /* Check Error conditions */
-  if (a->count != 1) {
-    lval_del(a);
-    return lval_err("Function 'head' passed too many arguments!");
-  }
+  LASSERT(a, (a->count != 1), "Function 'head' passed too many arguments!");
 
-  if (a->cell[0]->type != LVAL_QEXPR) {
-    lval_del(a);
-    return lval_err("Function 'head' passed incorrect types!");
-  }
+  LASSERT(a, (a->cell[0]->type != LVAL_QEXPR), "Function 'head' passed incorrect types!");
 
-  if (a->cell[0]->count == 0) {
-    lval_del(a);
-    return lval_err("Function 'head' passed {}!");
-  }
+  LASSERT(a, (a->cell[0]->count == 0), "Function 'head' passed {}!");
 
   /* Otherwise take first argument */
   lval* v = lval_take(a, 0);
@@ -220,20 +213,11 @@ lval* builtin_head(lval* a) {
 
 lval* builtin_tail(lval* a) {
   /* Check Error conditions */
-  if (a->count != 1) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed too many arguments!");
-  }
+  LASSERT(a, (a->count != 1), "Function 'tail' passed too many arguments!");
 
-  if (a->cell[0]->type != LVAL_QEXPR) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed incorrect types!");
-  }
+  LASSERT(a, (a->cell[0]->type != LVAL_QEXPR), "Function 'tail' passed incorrect types!");
 
-  if (a->cell[0]->count == 0) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed {}!");
-  }
+  LASSERT(a, (a->cell[0]->count == 0), "Function 'tail' passed {}!");
 
   /* Take first argument */
   lval* v = lval_take(a, 0);
